1e17f1b82e385b81d8d426727d895128146c7bc1
Pavilion <Pavilion@WinsLee>
2022-12-12 19:00:42 +0700

    first commit

---
+++ src/ifelsegabungan/IfElseGabungan.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifelsegabungan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pavilion
+ */
+public class IfElseGabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in );
+        String pesawat,tujuan;
+        System.out.println("BANDARA ASYIAPPP");
+        System.out.println("-------------------");
+        System.out.println("Silahkan Masukkan Jenis Maskapai");
+        pesawat = input.next();
+        System.out.println("Kemana tujuan Anda : Jakarta , Surabaya , Bali");
+        tujuan = input.next();
+        if ("garuda".equalsIgnoreCase(pesawat))
+        {
+            if ("jakarta".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.1.200 ");
+            }
+            else if ("surabaya".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.600.000 ");
+            }
+            else if ("bali".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.800.000 ");
+            } else 
+            {
+                System.out.println("Maaf Inputan Anda Salah");
+            }
+        }
+        else if ("lion".equalsIgnoreCase(pesawat))
+        {
+            if("jakarta".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.800.000 ");
+            }
+            else if ("surabaya".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.400.000 ");
+            }
+            else if ("bali".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.600.000 ");
+            }
+            else 
+            {
+                System.out.println("Maaf Inputan Anda Salah");
+            }
+        }
+        else if ("sriwijaya".equalsIgnoreCase(tujuan))
+        {
+            if ("jakarta".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.700.000 ");
+            }
+            else if ("surabaya".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.420.000 ");
+            }
+            else if ("bali".equalsIgnoreCase(tujuan))
+            {
+                System.out.println(pesawat + ", Dengan Tujuan ke " + tujuan + " Dengan Harga Rp.550.000 ");
+            }
+            else
+            {
+                System.out.println("Maaaf Inputan Anda Salah");
+            }
+        }
+        else 
+        {
+            System.out.println("Maaf Inputan Anda Salah");
+        }
+    }
+    
+}
